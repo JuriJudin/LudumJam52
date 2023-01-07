ac603d555accbd755b74cdd7274eb5db5cee4782
Federico <fedekpo95@gmail.com>
2023-01-07 14:26:26 -0300

    Elementos basicos y menus

---
+++ LudumJam52/Assets/Scripts/menuStart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuStart : MonoBehaviour
+{
+    //protected ScoreManager scoremanager;
+    private Text final_puntaje;
+
+    void Start()
+    {
+        /* scoremanager = FindObjectOfType<ScoreManager>();
+        if (scoremanager)
+        {
+            final_puntaje = GameObject.Find("finalscore").GetComponent<Text>();
+            scoremanager.reset_puntaje();
+        } */
+    }
+
+    public void cambiarescena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    public void abrirpagina(string url)
+    {
+        SceneManager.LoadScene(url);
+    }
+
+}
