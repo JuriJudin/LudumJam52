81a5d2174709364488b5e3184bef84de98faef5d
Federico <fedekpo95@gmail.com>
2023-01-07 22:26:04 -0300

    Seguimiento de camara y ubicacion de planta fija arriba de la bola

--- LudumJam52/Assets/Scripts/sphere.cs
+++ LudumJam52/Assets/Scripts/sphere.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class sphere : MonoBehaviour
 {
-    public float speed = 2;
+    public float speed = 0.4f;
+    public float maxSpeed = 1.2f;
+    public Transform cameraTransform;
+    public Transform plantTransform;
+    public float distanceFromCamera = 10;
     private Rigidbody rb;
+    private MeshRenderer renderer;
+    private float ballheight;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        renderer = GetComponent<MeshRenderer>();
+        ballheight = renderer.bounds.size.y;
     }
 
     // Update is called once per frame
@@ -21,5 +29,12 @@ public class sphere : MonoBehaviour
         Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
 
         rb.AddForce(movement * speed);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        Vector3 objectpos = new Vector3(transform.position.x,transform.position.y+5.0f, transform.position.z-5.0f);
+        cameraTransform.position = objectpos;
+
+        objectpos = new Vector3(transform.position.x,transform.position.y+ballheight, transform.position.z);
+        plantTransform.position = objectpos;
     }
 }
